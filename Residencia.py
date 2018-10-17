23ca4ddd3b37d4ad77ee5256f5505348cfd3f05b
Eric Suarez <ericsuarezgallareta1996@gmail.com>
2018-10-17 04:10:31 -0500

    Visualizacion del dataframe en el modo grafico

--- Residencia.py
+++ Residencia.py
@@ -1,71 +1,20 @@
-"""
-import tkinter as tk
-class main(tk.Tk):
-
-    def __init__(self, *args, **kwargs):
-
-        
-        tk.Tk.__init__(self, *args, **kwargs)
-        container = tk.Frame(self)
-        
-        
-
-        container.pack(side="top",fill="both", expand=True)
-
-        container.grid_rowconfigure(0,weight=1)
-        container.grid_columnconfigure(0,weight=1)
-
-        self.frames = {}
-
-        frame = Startpage(container, self)
-        self.frames[Startpage] = frame
-        frame.grid(row=0, column = 0, sticky="nsew")
-        self.show_frame(Startpage)
-    
-    def show_frame(self,count):
-        frame = self.frames[count]
-        frame.tkraise()
-
-class Startpage(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self,parent)
-        path = tk.PhotoImage(file = "logo_tec.png")
-        logo = tk.Label(self, image= path)
-        logo.pack(pady = 5,ipady=5)      
-        etiquetatitulo = tk.Label(self, text= "Bienvenidos")
-        etiquetatitulo.config(font=("Arial", 25))
-        etiquetatitulo.pack(pady=10, padx=10)
-        etiquetatitulo2 = tk.Label(self, text="Sistema para el analisis de datos climaticos")
-        etiquetatitulo2.config(font=("Arial", 20))
-        etiquetatitulo2.pack(padx=5, pady = 5, ipadx=5,ipady=5)
-        tk.Button(self, text="Iniciar", width=20).pack(padx=5, pady =10)
-        tk.Button(self, text="Salir", width=20).pack(padx=5, pady =10)
-
-app=main()
-app.title("Analisis de datos climaticos")
-app.geometry("600x350") 
-app.iconbitmap("logo_tec.ico")
-app.mainloop()
-
-
-
-
-"""
-
 from tkinter import *
+from tkinter import filedialog
+import re
+import pandas as pd
+import matplotlib.pyplot as tls
 root = Tk()
 
 root.title("Analisis de datos climaticos")
 root.resizable(1,1)
-root.geometry("600x350")
-root.iconbitmap("logo_tec.ico")
-
-#comandos para los botones
 
 #comando para salir
 def salir():
     root.quit()
+
+
+
+
 
 #comando abrir nueva ventana
 def cargar_archivos():
@@ -73,22 +22,61 @@ def cargar_archivos():
     abrir_data=Tk()
     abrir_data.title("Analisis de datos climaticos")
     abrir_data.resizable(1,1)
-    abrir_data.geometry("600x350")
-    abrir_data.iconbitmap("logo_tec.ico")
     frame2 = Frame(abrir_data, width=250, height=250)
-    frame2.grid(row=0, colum=1)
-    frame3 = Frame(abrir_data, width=250, height=250)
-    frame3.grid(row=0, colum=2)
+    frame2.grid(row=0, column=1)
     titulo2=Label(frame2, text="Seleccione el archivo" )
-    titulo2.config(font=("Arial", 15))
+    titulo2.config(font=(15))
+    titulo2.grid(padx=5, pady =5, row=0, column=0)
+    
+    #abrir cuadro de seleccion del archivo
+    def abrir_dialog():
+        ruta=filedialog.askopenfilename(title='Seleccionar el archivo de informacion', filetypes=(("Archivos Data","*.dat"),("Todos los Archivos","*.*")))
+        pathlabel=Label(frame2)
+    
+        pathlabel.config(text=ruta)
+        pathlabel.grid(row=1,column=0)
 
-    titulo2.grid(padx=5, pady =5, row=0, column=3)
-    direccion = Entry(frame2, width=40)
-    direccion.config()
-    direccion.grid(padx=5, pady=5, ipadx=5, ipady=5, row=1, column=3 ) 
-    file_choosser= Button(frame2, text="Seleccionar Archivo" ,width=20)
-    file_choosser.config()
-    file_choosser.grid(padx=5, pady=5, ipadx=5, ipady=5, row=1, column=4, side="center")
+        #ver registros de la ruta seleccionada
+        def ver_mas():
+            array1year=[]
+            array1month=[]
+            array2=[]
+            #limpieza de datos
+            for line in open(ruta):
+                array1year.append(line[0:4])
+                array1month.append(line[4:6])
+                if line[7]=='-':
+                    array2.append(line[7:11])
+        
+                else:
+                    array2.append(line[7:10])
+  
+            lista = []
+            lista2 = []
+            listaGeneral = []
+            for valor in array2:
+                lista.append(valor.rstrip('\n'))
+
+
+
+            for sal in range(0, 623):
+                lista2.append(array1year[sal] +'-'+array1month[sal])
+
+            #creación del dataframe
+            listaGeneral = dict(zip(lista2[1:], lista[1:]))
+            tupla = listaGeneral.items()
+            df = pd.DataFrame(list(tupla))
+            print(df)        
+        
+        ver_reg= Button(frame2, text="Ver registros", width=20, command=ver_mas)
+        ver_reg.grid(padx=5, pady=5, ipadx=5, ipady=5,row=2, column=1)
+         
+
+    file_choosser= Button(frame2, text="Seleccionar Archivo" ,width=20, command=abrir_dialog)
+    file_choosser.grid(padx=5, pady=5, ipadx=5, ipady=5, row=1, column=1)
+   
+    
+    
     root.iconify()
 
 
@@ -113,10 +101,10 @@ path = PhotoImage(file = "logo_tec.png")
 logo = Label(frame, image= path, width= 100, height=100)
 logo.pack(pady = 5,ipady=5)
 etiquetatitulo = Label(frame, text= "Bienvenidos")
-etiquetatitulo.config(font=("Arial", 25))
+etiquetatitulo.config(font=(25))
 etiquetatitulo.pack(pady = 5,ipady=5)
 etiquetatitulo2 = Label(frame, text="Sistema para el analisis de datos climaticos")
-etiquetatitulo2.config(font=("Arial", 20))
+etiquetatitulo2.config(font=(20))
 etiquetatitulo2.pack(padx=5, pady = 5, ipadx=5,ipady=5)
 Button(frame, text="Iniciar", width=20, command=cargar_archivos).pack(padx=5, pady =10)
 Button(frame, text="Salir", width=20, command=salir).pack(padx=5, pady =10)
