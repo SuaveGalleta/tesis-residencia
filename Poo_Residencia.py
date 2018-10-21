752395be15edb34bdfed2bbeeb56d145317f06c7
Eric Suarez <ericsuarezgallareta1996@gmail.com>
2018-10-20 19:31:38 -0500

    Comandos botones menu

--- Poo_Residencia.py
+++ Poo_Residencia.py
@@ -1,4 +1,9 @@
 import tkinter as tk
+from tkinter import filedialog
+
+
+
+
 
 class AnalisisClima(tk.Tk):
 
@@ -10,13 +15,51 @@ class AnalisisClima(tk.Tk):
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
         
-        self.frames={}
+        #Asignación del titulo de la ventana principal
+        self.title("Analisis de Datos Climaticos")
         
-        frame= Inicio(container, self)
 
-        self.frames[Inicio] = frame
+    
+            
 
-        frame.grid(row=0, column=0, sticky="nsew")
+
+        #Creacion del menu
+        menubar = tk.Menu(container)
+        tk.Tk.config(self, menu=menubar)/
+        #Asigando las diferentes opciones del menu
+        filemenu= tk.Menu(menubar, tearoff=0)
+        filemenu.add_command(label="Open File...", command=lambda:self.show_frame(Show_data))
+        filemenu.add_separator()
+        filemenu.add_command(label="Exit", command=self.quit)
+
+        
+        view= tk.Menu(menubar, tearoff=0)
+        view.add_command(label="Show Graph")
+        view.add_separator()
+        view.add_command(label="EMD")
+
+
+        ayuda = tk.Menu(menubar, tearoff=0)
+        ayuda.add_command(label="Dodumentation")
+        ayuda.add_command(label="About")
+
+        #Mostrar en la ventana el menu
+        menubar.add_cascade(label="File", menu=filemenu)
+        menubar.add_cascade(label="View", menu=view)
+        menubar.add_cascade(label="Help", menu=ayuda)
+
+
+        
+        self.frames={}
+
+        for F in (Inicio, Show_data):
+
+        
+            frame= F(container, self)
+
+            self.frames[F] = frame
+
+            frame.grid(row=0, column=0, sticky="nsew")
 
         self.show_frame(Inicio)
     
@@ -25,6 +68,11 @@ class AnalisisClima(tk.Tk):
          
         frame = self.frames[cont]
         frame.tkraise()
+
+    
+  
+    
+    
 
 #Ventana de inicio
 class Inicio(tk.Frame):
@@ -40,7 +88,21 @@ class Inicio(tk.Frame):
         welcome.pack(pady=5, padx=10,ipady=5, ipadx=10)
         maintitle=tk.Label(self, text="Analisis de datos climaticos", font=("Arial",25))
         maintitle.pack(pady=10, padx=10,ipady=5, ipadx=5)
+
+
+class Show_data(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self,parent)
+        ruta=filedialog.askopenfilename(title='Seleccionar el archivo de informacion', filetypes=(("Archivos Data","*.dat"),("Todos los Archivos","*.*")))
+        print(ruta)
+    
         
+        
+        
+
+        
+
 
 app= AnalisisClima()
 app.mainloop()
