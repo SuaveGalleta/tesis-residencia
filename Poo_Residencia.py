33fae46fed4fa11a0855824a3e21342443cf9972
Eric Suarez <ericsuarezgallareta1996@gmail.com>
2018-10-22 01:05:11 -0500

    Programa Terminado, agregando personalizacion

--- Poo_Residencia.py
+++ Poo_Residencia.py
@@ -14,6 +14,7 @@ from PyEMD import EMD
 import numpy as np
 import re
 import scipy.io
+from tkinter import simpledialog
 
 
 #global ruta_new
@@ -120,18 +121,24 @@ class Show_data(tk.Frame):
                 array1year.append(line[0:4])
                 array1month.append(line[4:6])
                 if line[7]=='-':
-                    array2.append(line[7:11])
+                    array2.append(line[7:])
+                    
                 else:
-                    array2.append(line[7:10])
+                    array2.append(line[7:])
+                    
+                    
   
             lista = []
             lista2 = []
             listaGeneral = []
             for valor in array2:
-                lista.append(valor.rstrip('\n'))          
-            for sal in range(0, 623):
+                lista.append(valor.rstrip('\n'))
+                #print(lista)          
+            for sal in range(len(lista)):
                 lista2.append(array1year[sal] +'-'+array1month[sal])
+                #print(lista2)
             #creación del dataframe
+            
             listaGeneral = dict(zip(lista2[1:], lista[1:]))
             tupla = listaGeneral.items()
             df = pd.DataFrame(list(tupla))
@@ -181,25 +188,40 @@ class Show_graph(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
+        
+        tk.Label(self, text="Grafica", font=("Arial",20)).pack()
+        frame_bonito = tk.Frame(self)
+        frame_bonito.pack()
 
+        def popup():
+           tl= simpledialog.askstring("Prueba 1", "Ingrese el titulo de la Grafica")
+           lx= simpledialog.askstring("Prueba 2", "Ingrese nombre para el eje x")
+           ly= simpledialog.askstring("Prueba 3", "Ingrese nombre para el eje y")
+
+           print(tl, lx, ly )
+
+
+        ttk.Button(frame_bonito,text="Configurar", command=popup).grid(row=0, column= 1,padx=5, pady =5, ipadx=5, ipady=5)
         #Funcion para realizar la grafica
         def ver_grafica():
             a.plot(my_graph)
-            a.set_title("ONI")
-            a.set_xlabel("Años")
-            a.set_ylabel("Datos ONI")
-            a.set_xticks([0,100,200,300,400,500,600], [1996,1970,1980,1990,2000,2015,2017])
-            a.set_xticklabels([0,1996,1970,1980,1990,2000,2015,2017])
+            #a.set_title("ONI")
+            #a.set_xlabel("Años")
+            #a.set_ylabel("Datos ONI")
+            #a.set_xticks([0,100,200,300,400,500,600], [1996,1970,1980,1990,2000,2015,2017])
+            #a.set_xticklabels([0,1996,1970,1980,1990,2000,2015,2017])
             
             canvas = FigureCanvasTkAgg(f, self)
             canvas.show()
             canvas.get_tk_widget().pack(side = tk.TOP, fill = tk.BOTH, expand = True)
 
-            ttk.Button(self,text="Aplicar EMD", command=lambda:controller.show_frame(Show_emd)).pack(padx=5, pady =5, ipadx=5, ipady=5)
+           
         
-        tk.Label(self, text="Grafica", font=("Arial",20)).pack()
+        
         #boton desplegar la grafica
-        ttk.Button(self,text="Desplegar grafica", command=ver_grafica).pack(padx=5, pady =5, ipadx=5, ipady=5)
+        
+        ttk.Button(frame_bonito,text="Desplegar grafica", command=ver_grafica).grid(row=0, column= 0,padx=5, pady =5, ipadx=5, ipady=5)
+        ttk.Button(frame_bonito,text="Aplicar EMD", command=lambda:controller.show_frame(Show_emd)).grid(row=0, column= 2,padx=5, pady =5, ipadx=5, ipady=5)
 
 
 #ventana para ver el EMD
@@ -207,38 +229,65 @@ class Show_emd(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self,parent)     
+        
+        
+        
+        #barraframe = tk.Scrollbar(self)
+        #barraframe.pack(side=tk.RIGHT, fill= tk.Y)
+        #self.config(yscrollcommand=barraframe.set)
+        #creacion de la vista de los datos
+        #mylist = ttk.Treeview(self, columns=("valor"))            
+        #barra = tk.Scrollbar(self, command=mylist.yview)
+        #barra.pack( side = tk.RIGHT, fill = tk.Y )
+        #mylist.configure(yscrollcommand=barra.set)
+        #mylist.pack()
+
 
         def desplegar_emd():
 
             dataoni = []
             for item in new_list:
                 dataoni.append(float(item))
-            x = np.linspace(0,10,623)
             signal = np.array(dataoni)
             emd = EMD()
             IMFS = emd.emd(signal)
-            N = IMFS.shape[0]+1
             funcion= tk.StringVar()
             funcion.set("Seleccione la Descomposición")
 
             opciones = []
             for line in range(len(IMFS)):
                 opciones.append(line)
-                print (line)            
+                #print (line)            
             
             dropselect = ttk.OptionMenu(self, funcion, *opciones)
             dropselect.pack(padx=5, pady =5, ipadx=5, ipady=5)
 
+         
+            
             def ver_dme():
                 seleccion= int(funcion.get())
-                a.clear()
+                global my_canvas           
+                a.clear()            
                 a.plot(IMFS[seleccion])
-                canvas = FigureCanvasTkAgg(f, self)
-                canvas.show()
-                canvas.get_tk_widget().pack(side = tk.TOP, fill = tk.BOTH, expand = True)
+                a.set_title("IMF "+str(seleccion))
+                a.set_xlabel("Tiempo [s]")
+                canvas2 = FigureCanvasTkAgg(f, self)
+                canvas2.show()
+                canvas2.get_tk_widget().pack(side = tk.TOP, fill = tk.BOTH, expand = True)
+                my_canvas = canvas2
+            def clear2():
+                my_canvas.get_tk_widget().destroy()    
+ 
                 
+            
+            
+                
+            my_frame_grid=tk.Frame(self)
+            my_frame_grid.pack()    
 
-            tk.Button(self, text="Desplegar grafica", command=ver_dme).pack()
+            tk.Button(my_frame_grid, text="Desplegar grafica", command=ver_dme).grid(row=0, column=0,padx=5, pady =5, ipadx=5, ipady=5)
+            tk.Button(my_frame_grid, text="Limpiar Pantalla", command=clear2).grid(row=0, column=1,padx=5, pady =5, ipadx=5, ipady=5)
+            
 
             #a.plot(IMFS[5])
 
@@ -253,6 +302,11 @@ class Show_emd(tk.Frame):
                 #tls.xlabel("Time [s]")
     
         tk.Button(self, text="Desplegar DME", command=desplegar_emd).pack(padx=5, pady =5, ipadx=5, ipady=5)
+        #tk.Button(self, text="Limpiar Pantalla", command=).pack()
+
+
+
+    
 
 app= AnalisisClima()
 app.mainloop()
